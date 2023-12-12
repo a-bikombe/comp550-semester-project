f4e4ccb0b331c41117bd973beb5a90d84974363e
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2023-12-11 20:03:06 -0500

    edit

--- semester-project.py
+++ semester-project.py
@@ -22,7 +22,6 @@ router2 = 0;
 
 # Send packets (user input for which city they are in (randomly choose between 3 cities to assign to user's device) and which they are sending to (choose between 3 - 1 device in each city))
 
-
 def send():
 	# "Y/N to connect to server"
 	# Acknowledgement print: "Your device is a [device1/2/3] in [city1/2/3]"
