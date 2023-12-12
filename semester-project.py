64f66c0eac71b456b5a38e494cc8e655a421f6f9
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2023-12-11 19:33:10 -0500

    init

---
+++ semester-project.py
@@ -0,0 +1,4 @@
+# Author: Arianna Bikombe
+# Date: December 11, 2023
+# Purpose: My semester project will be a Python program simulating the packet transfer between two devices. The options for fictional devices that the user can choose from will originate from different cities, and the program will choose the best router to transfer data between the devices, this being the closest and therefore most efficient router between the two cities/devices.
+
